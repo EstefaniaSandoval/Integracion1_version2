ec51d9dff119bed13a52607fd8c4f1b4812344bf
Diego24Vas <dvasquez2024@alu.uct.cl>
2024-11-22 21:53:57 -0300

    se coloco footer en pie de pagina

--- Integracion_I/estructura/php/ver_registros_vehiculos.php
+++ Integracion_I/estructura/php/ver_registros_vehiculos.php
@@ -108,61 +108,6 @@
     </div>
 </div>
 
-<!-- Footer -->
-<footer>
-    <!-- Contenido del footer tal cual estaba -->
-    <div class="footer">
-        <!-- Redes sociales -->
-        <div class="footer-column">
-            <h3>Redes Sociales</h3>
-            <div class="social-icons">
-                <a href="https://www.instagram.com/uctemuco/"><img src="../img/footer/instagram.png" alt="Instagram"></a>
-                <a href="https://www.facebook.com/canaluctemuco"><img src="../img/footer/facebook.png" alt="Facebook"></a>
-                <a href="https://twitter.com/uctemuco"><img src="../img/footer/Twitter.png" alt="Twitter"></a>
-                <a href="https://www.youtube.com/user/canaluctemuco"><img src="../img/footer/youtube.png" alt="Youtube"></a>
-            </div>
-        </div>
-
-        <!-- Contacto -->
-        <div class="footer-column">
-            <h3>Contáctanos</h3>
-            <div class="contactos">
-                <a href="mailto:info@uct.cl">
-                    <img src="../img/footer/correo.png" alt="Correo"> info@uct.cl
-                </a>
-                <a href="https://maps.app.goo.gl/UpF5DKgBwauTYQys8">
-                    <img src="../img/footer/ubicacion.png" alt="Ubicación"> Rudecindo Ortega 2959, Temuco
-                </a>
-            </div>
-        </div>
-
-        <!-- Información -->
-        <div class="footer-column">
-            <h3>Información</h3>
-            <div class="info">
-                <a href="terminos.php">
-                    <img src="../img/footer/terminos.png" alt="Términos y Condiciones"> Términos y Condiciones
-                </a>
-                <a href="https://www.uct.cl/">
-                    <img src="../img/footer/oficial.png" alt="Página oficial UCT"> Página oficial UCT
-                </a>
-            </div>
-        </div>
-
-        <!-- Logo -->
-        <div class="footer-column">
-            <div class="ing-logo">
-                <img src="../img/footer/uct_inf.png" alt="inf">
-            </div>
-        </div>
-    </div>
-
-    <!-- Derechos reservados -->
-    <div class="derechos-reservados">
-        &copy; 2024 Universidad Católica de Temuco. Todos los derechos reservados.
-    </div>
-</footer>
-
 <script>
     document.getElementById('filtro-principal').addEventListener('change', function () {
         const filtro = this.value; // Obtiene el filtro seleccionado
